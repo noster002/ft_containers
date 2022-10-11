ec3120ff48e4b32426ea19d8f0c13e85d9123870
Nils Ostermeier <nosterme@c2s17d109.42wolfsburg.de>
2022-10-11 19:14:39 +0200

    is_integral prototype

--- is_integral.hpp
+++ is_integral.hpp
@@ -6,29 +6,53 @@
 /*   By: nosterme <nosterme@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 18:26:18 by nosterme          #+#    #+#             */
-/*   Updated: 2022/10/10 18:49:33 by nosterme         ###   ########.fr       */
+/*   Updated: 2022/10/11 19:13:07 by nosterme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef IS_INTEGRAL_HPP
 # define IS_INTEGRAL_HPP
 
-template< typename T, T Val >
-struct integral_constant
+template< bool Val >
+struct integral_constant_bool
 {
 
-	typedef T							value_type;
-	typedef integral_constant< T, Val >	type;
+	static bool										value = Val;
+	typedef bool									value_type;
+	typedef struct integral_constant_bool< Val >	type;
 
-}
+	operator	value_type( void ) const
+	{
+		return ( value );
+	}
 
-typedef struct integral_constant< bool, true >	true_type;
-typedef struct integral_constant< bool, false >	false_type;
+};
+
+typedef struct integral_constant_bool< true >	true_type;
+typedef struct integral_constant_bool< false >	false_type;
 
 namespace ft
 {
 
+	template< typename T > struct is_integral : public false_type {};
+	template< typename T > struct is_integral< T const > : public is_integral< T > {};
+	template< typename T > struct is_integral< T volatile > : public is_integral< T > {};
+	template< typename T > struct is_integral< T const volatile > : public is_integral< T > {};
 
+	template<> struct is_integral< bool > : public true_type {};
+
+	template<> struct is_integral< char > : public true_type {};
+	template<> struct is_integral< wchar_t > : public true_type {};
+	template<> struct is_integral< signed char > : public true_type {};
+	template<> struct is_integral< unsigned char > : public true_type {};
+
+	template<> struct is_integral< short int > : public true_type {};
+	template<> struct is_integral< int > : public true_type {};
+	template<> struct is_integral< long int > : public true_type {};
+
+	template<> struct is_integral< unsigned short int > : public true_type {};
+	template<> struct is_integral< unsigned int > : public true_type {};
+	template<> struct is_integral< unsigned long int > : public true_type {};
 
 }
 
