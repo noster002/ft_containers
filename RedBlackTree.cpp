6b6fda4799cd442ed28d4ad00b0753ca40e424c0
nosterme <nosterme@student.42wolfsburg.de>
2022-11-09 01:50:25 +0100

    working on rb_tree_rebalance_for_erase, node has two NIL nodes finished

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -6,7 +6,7 @@
 /*   By: nosterme <nosterme@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 13:48:06 by nosterme          #+#    #+#             */
-/*   Updated: 2022/11/08 17:51:02 by nosterme         ###   ########.fr       */
+/*   Updated: 2022/11/09 01:20:23 by nosterme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	rb_tree_rotate_left( rb_tree_node_base * const lhs,\
 							 rb_tree_node_base *& root )
 {
-	if ( ( lhs == NULL ) || ( lhs->left == NULL ) )
+	if ( ( lhs == 0 ) || ( lhs->m_right == 0 ) )
 		return ;
 	rb_tree_node_base * const	rhs = lhs->m_right;
 
@@ -38,7 +38,7 @@ void	rb_tree_rotate_left( rb_tree_node_base * const lhs,\
 void	rb_tree_rotate_right( rb_tree_node_base * const rhs,\
 							  rb_tree_node_base *& root )
 {
-	if ( ( rhs == NULL ) || ( rhs->left == NULL ) )
+	if ( ( rhs == 0 ) || ( rhs->m_left == 0 ) )
 		return ;
 	rb_tree_node_base * const	lhs = rhs->m_left;
 
@@ -142,4 +142,86 @@ void	rb_tree_insert_and_rebalance( bool const insert_left,\
 	}
 	root->m_color = black;
 	return ;
+}
+
+rb_tree_node_base *	rb_tree_rebalance_for_erase( rb_tree_node_base * const node,\
+												 rb_tree_node_base & head )
+{
+	rb_tree_node_base *&	root = head.m_parent;
+
+	// node has two NIL nodes
+
+	if ( node == node->m_parent->m_left )
+	{
+		if ( node->m_color == black )
+		{
+			rb_tree_node_base *	sibling = node->m_parent->m_right;
+
+			if ( sibling->m_color == red )
+			{
+				rb_tree_rotate_left( node->m_parent, root );
+				node->m_parent->m_parent->m_color = black;
+				sibling = node->m_parent->m_right;
+				if ( sibling == 0 )
+					node->m_parent->m_color = red;
+				else
+					sibling->m_color = red;
+			}
+			else
+			{
+				if ( sibling->m_right )
+				{
+					rb_tree_rotate_left( node->m_parent, root );
+					node->m_parent->m_parent->m_color = node->m_parent->m_color;
+					sibling->m_right->m_color = black;
+					node->m_parent->m_color = black;
+				}
+				else
+				{
+					rb_tree_rotate_right( sibling, root );
+					rb_tree_rotate_left( node->m_parent, root );
+					node->m_parent->m_parent->m_color = node->m_parent->m_color;
+					node->m_parent->m_color = black;
+				}
+			}
+		}
+		node->m_parent->m_left = 0;
+	}
+	else
+	{
+		if ( node->m_color == black )
+		{
+			rb_tree_node_base *	sibling = node->m_parent->m_left;
+
+			if ( sibling->m_color == red )
+			{
+				rb_tree_rotate_right( node->m_parent, root );
+				node->m_parent->m_parent->m_color = black;
+				sibling = node->m_parent->m_left;
+				if ( sibling == 0 )
+					node->m_parent->m_color = red;
+				else
+					sibling->m_color = red;
+			}
+			else
+			{
+				if ( sibling->m_left )
+				{
+					rb_tree_rotate_right( node->m_parent, root );
+					node->m_parent->m_parent->m_color = node->m_parent->m_color;
+					sibling->m_left->m_color = black;
+					node->m_parent->m_color = black;
+				}
+				else
+				{
+					rb_tree_rotate_left( sibling, root );
+					rb_tree_rotate_right( node->m_parent, root );
+					node->m_parent->m_parent->m_color = node->m_parent->m_color;
+					node->m_parent->m_color = black;
+				}
+			}
+		}
+		node->m_parent->m_right = 0;
+	}
+	return ( node );
 }
